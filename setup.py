7b4aa8ad02fccfe9d373f64556f3bcf264f4d9c1
Kartik G <gk2295006@gmail.com>
2026-08-07 17:25:03 +0530

    structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Kartik Shivakumar Gadad",
+    author_email="kartikshivakumargadad@gmail.com",
+    packages=find_packages(),
+)
